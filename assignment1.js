4a1cc0cee577ff3edfbe6e5472cb1b4825ae43a4
Jorge Pérez Rubio <jorgeperezrubio@MacBook-Air-de-Jorge.local>
2020-03-06 10:44:04 +0100

    Exam1

--- assignment1.js
+++ assignment1.js
@@ -1,4 +1,5 @@
 (function(){
+	'use strict'
 	
 	angular.module("DataApp", [])
 	
@@ -7,9 +8,10 @@
 	DataController.$inject = ['$scope','$filter','$window'];
 	
 	function DataController($scope, $filter, $window){
-		
+		$scope.message = "";
 		$scope.countNames = 0;
 		$scope.listLunch = "";
+		$scope.names = [];
 		
 		$scope.analyzeList = function (){
 			if ($scope.listLunch == "") {
@@ -20,18 +22,18 @@
 				$scope.countNames = 0;
 				
 				for (i=0; i<$scope.names.length; i++){
-					if ($scope.names[i].trim() != "") $scope.countNames++
+					if ($scope.names[i].trim() != "") $scope.countNames++;
 				}
 			}
 			
 			if ($scope.countNames == 0) { 
-				$scope.message = "Please enter data first"
+				$scope.message = "Please enter data first";
 			}
 			else if ($scope.countNames <= 3){
-				$scope.message = "Enjoy!"
+				$scope.message = "Enjoy!";
 			}
 			else {
-				$scope.message = "Too much!"
+				$scope.message = "Too much!";
 			}
 		};	
 	};
